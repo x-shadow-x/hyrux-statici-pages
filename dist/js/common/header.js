d1833892cabeb7bbc77a9e0442a0735b3226551d
x-shadow-x <2509344578@qq.com>
2017-03-28 00:05:57 +0800

    完成头部尾部的响应式编写

--- dist/js/common/header.js
+++ dist/js/common/header.js
@@ -1,93 +1,7 @@
 $(function() {
 
 	var clientWidth = $(window).width();
-	bodyHtmlEl = $('body, html'),
-		toTopEl = $('#toTop');
-
-	$(window).scroll(function() {
-		var scrollDis = $(window).scrollTop();
-		var lY = scrollDis * 0.1;
-		var cY = scrollDis * 0.2;
-		var rY = scrollDis * 0.3;
-		$('#bgPicLeft').css('transform', 'translate3D(0, ' + lY + 'px, 0)');
-		$('#bgCenter').css('transform', 'translate3D(0, ' + cY + 'px, 0)');
-		$('#bgRight').css('transform', 'translate3D(0, ' + rY + 'px, 0)');
-
-
-		var scrollTop = document.body.scrollTop || bodyHtmlEl.scrollTop();
-
-		if (scrollTop == 0) {
-			toTopEl.hide();
-		} else {
-			toTopEl.show();
-		}
-	});
-
-	$('#toTop').click(function() {
-		bodyHtmlEl.animate({
-			scrollTop: 0
-		}, 320);
-		toTopEl.hide();
-	});
-
-	$('#shareToBtn').click(function(e) {
-		e.stopPropagation();
-		$('#languageList').slideUp();
-		$('#fontSizeList').slideUp();
-		var shareToListtEl = $('#shareToList');
-		if (shareToListtEl.is(':visible')) {
-			shareToListtEl.slideUp();
-		} else {
-			shareToListtEl.slideDown();
-		}
-	});
-
-	$('#moreShareToList').on('click', 'a', function() {
-		$('#moreShareToList').slideUp();
-	});
-
-	$('#fontSizeBtn').click(function(e) {
-		e.stopPropagation();
-		$('#shareToList').slideUp();
-		$('#languageList').slideUp();
-		var fontSizeListEl = $('#fontSizeList');
-		if (fontSizeListEl.is(':visible')) {
-			fontSizeListEl.slideUp();
-		} else {
-			fontSizeListEl.slideDown();
-		}
-	});
-
-	$('#languageBtn').click(function(e) {
-		e.stopPropagation();
-		$('#shareToList').slideUp();
-		$('#fontSizeList').slideUp();
-		var languageListEl = $('#languageList');
-		if (languageListEl.is(':visible')) {
-			languageListEl.slideUp();
-		} else {
-			languageListEl.slideDown();
-		}
-	});
-
-	$('#fontSizeList').on('click', 'li', function(e) {
-		e.stopPropagation();
-		$(this).addClass('active').siblings('li').removeClass('active');
-		$('#fontSizeList').slideUp();
-		document.documentElement.style.fontSize = $(this).attr('data-size');
-	});
-
-	$('#languageList').on('click', 'li', function(e) {
-		e.stopPropagation();
-		$(this).addClass('active').siblings('li').removeClass('active');
-		$('#languageList').slideUp();
-	});
-
-	$(window).click(function() {
-		$('#shareToList').slideUp();
-		$('#fontSizeList').slideUp();
-		$('#languageList').slideUp();
-	});
+	bodyHtmlEl = $('body, html');
 
 	if (clientWidth > 768) {
 		$('#moreShareToBtn').mouseenter(function() {
@@ -102,28 +16,18 @@ $(function() {
 		});
 	} else {
 		$('#headerMenuBtn').click(function(e) {
-			if (e.target.id != 'headerMenuBtn') {
-				return;
-			}
 			var headerMenuMobileBox = $('#headerMenuMobileBox');
 			if (headerMenuMobileBox.is(':visible')) {
 				headerMenuMobileBox.hide();
-				$(this).removeClass('closed');
+				$('#headerMenuBtn').find('.fa').addClass('fa-bars').removeClass('fa-times');
 			} else {
 				headerMenuMobileBox.show();
-				$(this).addClass('closed');
+				$('#headerMenuBtn').find('.fa').addClass('fa-times').removeClass('fa-bars');
 			}
 		});
-		$('#moreShareToBtn').click(function(e) {
+		$('#headerMenuMobileBox').click(function(e) {
 			e.stopPropagation();
-			var moreShareToListEl = $('#moreShareToList');
-			if (moreShareToListEl.is(':visible')) {
-				$('#moreShareToList').slideUp();
-			} else {
-				$('#moreShareToList').slideDown();
-			}
 		});
-
 	}
 
 })
